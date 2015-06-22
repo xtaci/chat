6a204cb116fbc050f009bad58ce3aa7da09a71f1
xtaci <daniel820313@gmail.com>
2015-06-22 15:17:00 +0000

    save

--- src/chat/service.go
+++ src/chat/service.go
@@ -20,43 +20,72 @@ const (
 	MAX_QUEUE_SIZE     = 128 // num of message kept
 )
 
-type Message struct {
-	From     int32
-	To       int32
-	FromName string
-	Body     string
-}
-
 type User struct {
-	Name  string
-	Inbox []Message
+	Inbox []Chat_Message
 }
 
 type Muc struct {
 	UserIds []int32
-	Inbox   []Message
+	Inbox   []Chat_Message
 }
 
 type server struct {
-	Users map[int32]*User
-	Mucs  map[int32]*Muc
+	users          map[int32]*User
+	mucs           map[int32]*Muc
+	chat_chan      chan Chat_Message
+	chat_chan_ctrl chan Chat_Param
+	muc_chan       chan Chat_Message
+	muc_chan_ctrl  chan Chat_Param
 	sync.RWMutex
 }
 
 func (s *server) init() {
-	s.Users = make(map[int32]*User)
-	s.Mucs = make(map[int32]*Muc)
+	s.users = make(map[int32]*User)
+	s.mucs = make(map[int32]*Muc)
 }
 
-func (s *server) Subscribe(ChatService_SubscribeServer) error {
+func (s *server) Subscribe(stream ChatService_SubscribeServer) error {
+	die := make(chan bool)
+	defer close(die)
+
+	go func() {
+		for {
+			select {
+			case msg <- s.chat_chan:
+			case ctrl <- s.chat_chan_ctrl:
+			case <-die:
+				return
+			}
+		}
+	}()
+
+	for {
+		in, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		s.chat_chan_ctrl <- in
+	}
 	return nil
 }
 
-func (s *server) MucSubscribe(ChatService_MucSubscribeServer) error {
+func (s *server) MucSubscribe(stream ChatService_MucSubscribeServer) error {
 	return nil
 }
 
-func (s *server) Send(context.Context, *Chat_Message) (*Chat_Nil, error) {
+func (s *server) Send(ctx context.Context, msg *Chat_Message) (*Chat_Nil, error) {
+	switch msg.Type {
+	case Chat_CHAT:
+		if u := s.users[msg.ToId]; u != nil {
+			u.Inbox = append(u.Inbox, *msg)
+		}
+		s.chat_chan <- *msg
+	case Chat_MUC:
+		if u := s.mucs[msg.ToId]; u != nil {
+			u.Inbox = append(u.Inbox, *msg)
+		}
+		s.muc_chan <- *msg
+	}
 	return nil, nil
 }
 
