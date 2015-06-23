ce809d769810c4a3f3a0f195cec37a7d47160cd3
xtaci <daniel820313@gmail.com>
2015-06-23 09:59:40 +0000

    update

--- src/chat/service_test.go
+++ src/chat/service_test.go
@@ -1,17 +1,20 @@
 package main
 
 import (
-	"log"
 	. "proto"
 	"testing"
-	"time"
 
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
+	"sync"
 )
 
 const (
 	address = "localhost:50008"
+)
+
+var (
+	wg sync.WaitGroup
 )
 
 func TestChat(t *testing.T) {
@@ -26,47 +29,59 @@ func TestChat(t *testing.T) {
 	// Contact the server and print out its response.
 	_, err = c.Reg(context.Background(), &Chat_Id{Id: 1})
 	if err != nil {
-		log.Printf("could not query: %v", err)
+		t.Logf("could not query: %v", err)
 	}
 
-	go send(&Chat_Message{Id: 1, Body: []byte("Hello")})
-	go recv(&Chat_Id{1})
+	const COUNT = 10
+	wg.Add(3)
+	go recv(&Chat_Id{1}, COUNT, t)
+	go recv(&Chat_Id{1}, COUNT, t)
+	go send(&Chat_Message{Id: 1, Body: []byte("Hello")}, COUNT, t)
+	wg.Wait()
 }
 
-func send(m *Chat_Message) {
+func send(m *Chat_Message, count int, t *testing.T) {
 	conn, err := grpc.Dial(address)
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		t.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 	c := NewChatServiceClient(conn)
 	for {
+		if count == 0 {
+			wg.Done()
+			return
+		}
 		_, err := c.Send(context.Background(), m)
 		if err != nil {
-			log.Printf("send err : %v", err)
+			t.Fatal(err)
 		}
-		log.Printf("send msg: %v", m)
-		time.Sleep(3 * time.Second)
+		t.Log("send:", m)
+		count--
 	}
 }
 
-func recv(chat_id *Chat_Id) {
+func recv(chat_id *Chat_Id, count int, t *testing.T) {
 	conn, err := grpc.Dial(address)
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		t.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 	c := NewChatServiceClient(conn)
 	stream, err := c.Subscribe(context.Background(), chat_id)
 	if err != nil {
-		log.Fatal(err)
+		t.Fatal(err)
 	}
 	for {
+		if count == 0 {
+			wg.Done()
+			return
+		}
 		message, err := stream.Recv()
 		if err != nil {
-			log.Printf("recv err :%v", err)
+			t.Fatal(err)
 		}
-		log.Print(message)
+		t.Log("recv:", message)
+		count--
 	}
-	log.Printf("recv msg end")
 }
